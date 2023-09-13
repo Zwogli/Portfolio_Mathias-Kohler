c801ae00d346724e64f22faeb81d59242b3e0c06
Zwogli <Mathiaskohler@web.de>
2023-09-13 11:39:26 +0200

    fix btn function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,34 +12,9 @@ export class HeaderComponent {
     let menu = document.getElementById('drop-down-menu');
     let hamburgerbar = document.getElementById('hamburgerbar');
     let body = document.getElementById('body');
+    
     menu.classList.toggle('transform');
     hamburgerbar.classList.toggle('transform--x');
     body.classList.toggle('stopScrollY');
 }
-
-  // ngOnInit(){
-  //   window.addEventListener("resize", this.checkWindowSize);
-  // }
-
-  // checkWindowSize() {
-  //   if(window.innerWidth >= 850 && !this.checkWidthDesktop){
-  //     this.checkWidthDesktop = true;
-  //     this.menuToggle('desktop');
-  //   }else if(window.innerWidth < 850 && this.checkWidthDesktop){
-  //     this.checkWidthDesktop = false;
-  //     this.menuToggle('mobile');
-  //   }
-  // }
-
-  // menuToggle(device:string){
-  //   let menu = document.getElementById('drop-down-menu');
-  //   if(device === 'mobile'){
-  //     menu.classList.toggle('transform');
-  //   }else  
-  //   if(device === 'desktop'){
-  //     menu.classList.remove('transform');
-  //   }  
-  // }
-
- 
 }
