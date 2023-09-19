4aaa0c4765583a5b7d7a50d48a45cad607b830f6
Zwogli <Mathiaskohler@web.de>
2023-09-19 11:36:04 +0200

    edit own form style

--- src/app/section-contact/section-contact.component.ts
+++ src/app/section-contact/section-contact.component.ts
@@ -13,31 +13,34 @@ export class SectionContactComponent {
   @ViewChild('sendButton') sendButton: ElementRef;
 
   async sendMail() {
-    const action = 'https://mathias-kohler.developerakademie.net/portfolio-mathias-kohler/send_mail-portfolio.php';
+    const phpUrl =
+      'https://mathias-kohler.developerakademie.net/portfolio-mathias-kohler/send_mail-portfolio.php';
+    const action = phpUrl;
     let nameField = this.nameField.nativeElement;
     let mailField = this.mailField.nativeElement;
     let messageField = this.messageField.nativeElement;
     let sendButton = this.sendButton.nativeElement;
+    let fd = new FormData();
 
     this.disableFields(nameField, mailField, messageField, sendButton);
+    if (this.checkedInputFields(nameField, mailField, messageField)) {
+      this.showMissedInputFields(nameField, mailField, messageField);
+    } 
+    else {
+      this.showFilledInputFields(nameField, mailField, messageField);
+      this.createFormData(fd, nameField, mailField, messageField);
 
-    //>>>>> show animation
-    
-    let fd = new FormData();
-    this.createFormData(fd, nameField, mailField, messageField);
-
-    /**send*/
-    await fetch(
-      action,
-      {
+      /**send*/
+      await fetch(action, {
         method: 'POST',
         body: fd,
-      }
-    );
+      });
 
-    //>>>>>>> show send message
+      //>>>>>>> show send message
 
-    this.enableFields(nameField, mailField, messageField, sendButton);
+      this.enableFields(nameField, mailField, messageField, sendButton);
+      this.cleareFields();
+    }
   }
 
   /**disable input*/
@@ -48,19 +51,78 @@ export class SectionContactComponent {
     sendButton.disable = true;
   }
 
+  /**check input-fields, if-statem*/
+  checkedInputFields(nameField, mailField, messageField) {
+    return (
+      nameField.value == '' || nameField.value == undefined || 
+      mailField.value == '' || mailField.value == undefined || 
+      messageField.value == '' || messageField.value == undefined
+    );
+  }
+
+  /**style missed input-field*/
+  showMissedInputFields(nameField, mailField, messageField) {
+    let lableNameField = document.getElementById('lable-input-name-missed');
+    let lableMailField = document.getElementById('lable-input-email-missed');
+    let lableMessageField = document.getElementById('lable-input-message-missed');
+
+    if (this.checkedSingleInputField(nameField)) {
+      this.styleMissedInput(nameField, lableNameField);
+    }else{
+      nameField.classList.add("filledInput");
+    } if(this.checkedSingleInputField(mailField)){
+      this.styleMissedInput(mailField, lableMailField);
+    }else{
+      mailField.classList.add("filledInput");
+    }
+     if(this.checkedSingleInputField(messageField)){
+      this.styleMissedInput(messageField, lableMessageField);
+    } else {
+      messageField.classList.add("filledInput-message");
+    }
+  }
+
+  /**check every input field value*/
+  checkedSingleInputField(inputField){
+    return (inputField.value == '' || inputField.value == undefined);
+  }
+
+  /**set style by missed input value*/
+  styleMissedInput(input, lable){
+    lable.style.display = 'flex'
+    if(input.id === 'input-message'){
+      input.classList.add("missedInput-message");
+    }else{
+      input.classList.add("missedInput");
+    }
+  }
+
+  /**style filled input-field*/
+  showFilledInputFields(nameField, mailField, messageField){
+    nameField.classList.add("filledInput");
+    mailField.classList.add("filledInput");
+    messageField.classList.add("filledInput-message");
+  }
+
   /**send informations*/
-  createFormData(fd, nameField, mailField, messageField){
+  createFormData(fd, nameField, mailField, messageField) {
     fd.append('name', nameField.value);
     fd.append('email', mailField.value);
     fd.append('message', messageField.value);
-    return 
+    return;
   }
-  
+
   /**enable input*/
   enableFields(nameField, mailField, messageField, sendButton) {
     nameField.disable = false;
     mailField.disable = false;
     messageField.disable = false;
     sendButton.disable = false;
+  }
+
+  /**reset value*/
+  cleareFields(){
+    let contactForm = this.contactForm.nativeElement;
+    contactForm.reset();
   }
 }
